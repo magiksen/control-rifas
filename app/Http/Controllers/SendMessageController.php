889d1eed07eddca7dc6e8c66597253d57556d21d
Magiksen <elemejore@gmail.com>
2024-03-12 11:37:00 -0400

    Fix whatsapp sender

--- app/Http/Controllers/SendMessageController.php
+++ app/Http/Controllers/SendMessageController.php
@@ -20,19 +20,29 @@ class SendMessageController extends Controller
         $numerotelf = Str::of($numerotelf)->substr(1);
 
 
-        // $imagenticket = asset($ticket->imagen);
+        $imagenticket = $ticket->imagen;
 
-        $imagenticket = "https://images.unsplash.com/photo-1431250620804-78b175d2fada?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9";
+        // $imagenticket = "https://images.unsplash.com/photo-1431250620804-78b175d2fada?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9";
 
         $twilio_whatsapp_number = getenv('TWILIO_WHATSAPP_NUMBER');
         $account_sid = getenv("TWILIO_SID");
         $auth_token = getenv("TWILIO_AUTH_TOKEN");
-        $message = "Gracias por participar en el sorte, aca esta su ticket";
+        $contentSid = getenv("TWILIO_CONTENT_SID");
+        $messagingServiceSid = getenv("TWILIO_MESSAGING_SERVICE_SID");
+        $message = "Gracias por participar en la Gran Rifa, este es su ticket de participacion";
         $recipient = "whatsapp:+58".$numerotelf;
 
 
         $client = new Client($account_sid, $auth_token);
-        return $client->messages->create($recipient, array('mediaUrl' => $imagenticket, 'from' => "whatsapp:$twilio_whatsapp_number", 'body' => $message));
+        $client->messages->create($recipient, array(
+            'contentSid' => $contentSid, 
+            'from' => $messagingServiceSid, 
+            'contentVariables' => json_encode([
+                "1" => $imagenticket,
+            ]),
+        ));
+
+        return redirect()->route('tickets.index')->with('success', 'Mensaje enviado correctamente');;
 
     }
 }
