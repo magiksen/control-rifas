744350dd2a864e5ff6611c11ce445f41be9c91df
Magiksen <elemejore@gmail.com>
2024-04-24 13:32:11 -0400

    Cambios de permisos en tickets y localizacion a espanol

--- resources/views/admin/ticket/index.blade.php
+++ resources/views/admin/ticket/index.blade.php
@@ -9,11 +9,13 @@
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                         <h4 class="mb-sm-0">Ver Tickets</h4>
 
+                        @if(auth()->user()->hasanyrole('Admin|SuperAdmin'))
                         <div class="ms-auto">
                             <div class="btn-group">
                                 <a href="{{ route('tickets.control') }}" class="btn btn-info">Crear imagen de Control</a>
                             </div>
                         </div>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -26,21 +28,38 @@
 
                             <h4 class="card-title">Lista de tickets</h4>
                             <p class="card-title-desc">Visualizar la lista de tickets registrados</p>
-                            
-                            <div class="row">
-                                @foreach($numeros as $numero)
-                                    <div class="col-xl-1 text-center m-2">
-                                    @if($numero->ticket_id == 0)
-                                    <a href="{{ route('tickets.create',$numero->numero) }}" type="button" class="btn btn-success waves-effect waves-light">{{ $numero->numero }}</a>
-                                    @elseif($numero->ticket_id !== 0 && $numero->ticket->pago == 0)
-                                    <a href="{{ route('ticket.show',$numero->ticket_id) }}" type="button" class="btn btn-warning waves-effect waves-light">{{ $numero->numero }}</a>
-                                    @else
-                                    <a href="{{ route('ticket.show',$numero->ticket_id) }}" type="button" class="btn btn-danger waves-effect waves-light">{{ $numero->numero }}</a>
-                                    @endif
-                                    </div> 
-                                @endforeach
-                            </div>
-                                    
+
+                            @if(auth()->user()->hasanyrole('Admin|SuperAdmin'))
+                                <div class="row">
+                                    @foreach($numeros as $numero)
+                                        <div class="col-xl-1 text-center m-2">
+                                            @if($numero->ticket_id == 0)
+                                                <a href="{{ route('tickets.create',$numero->numero) }}" type="button" class="btn btn-success waves-effect waves-light">{{ $numero->numero }}</a>
+                                            @elseif($numero->ticket_id !== 0 && $numero->ticket->pago == 0)
+                                                <a href="{{ route('ticket.show',$numero->ticket_id) }}" type="button" class="btn btn-warning waves-effect waves-light">{{ $numero->numero }}</a>
+                                            @else
+                                                <a href="{{ route('ticket.show',$numero->ticket_id) }}" type="button" class="btn btn-danger waves-effect waves-light">{{ $numero->numero }}</a>
+                                            @endif
+                                        </div>
+                                    @endforeach
+                                </div>
+                            @else
+                                <div class="row">
+                                    @foreach($numeros as $numero)
+                                        <div class="col-xl-1 text-center m-2">
+                                            @if($numero->ticket_id == 0)
+                                                <a href="#" type="button" class="btn btn-success waves-effect waves-light">{{ $numero->numero }}</a>
+                                            @elseif($numero->ticket_id !== 0 && $numero->ticket->pago == 0)
+                                                <a href="#" type="button" class="btn btn-secondary waves-effect waves-light disabled">{{ $numero->numero }}</a>
+                                            @else
+                                                <a href="{{ route('ticket.show',$numero->ticket_id) }}" type="button" class="btn btn-secondary waves-effect waves-light disabled">{{ $numero->numero }}</a>
+                                            @endif
+                                        </div>
+                                    @endforeach
+                                </div>
+                            @endif
+
+
                         </div>
                     </div>
                 </div>
